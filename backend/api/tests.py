9687063cc7a6ae067be9759ec8fe425a405c5840
Артём Берников <bernikovartemal@gmail.com>
2025-09-28 19:54:45 +0200

    1

---
+++ backend/api/tests.py
@@ -0,0 +1,26 @@
+"""Тесты."""
+
+from http import HTTPStatus
+
+from api import models
+from django.test import Client, TestCase
+
+
+class TaskiAPITestCase(TestCase):
+    """Таски-апи."""
+
+    def setUp(self):
+        """Начать."""
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """Проверка доступности списка задач."""
+        response = self.guest_client.get('/api/tasks/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        """Проверка создания задачи."""
+        data = {'title': 'Test', 'description': 'Test'}
+        response = self.guest_client.post('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
