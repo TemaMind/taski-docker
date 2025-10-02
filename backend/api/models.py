22848504320f732ff8bf1511f9304bc25d6957e4
Артём Берников <bernikovartemal@gmail.com>
2025-10-02 22:39:28 +0200

    force rebuild backend with migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,7 +7,7 @@ class Task(models.Model):
     """Модель-задача."""
 
     title = models.CharField(verbose_name='Заголовок', max_length=120)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
 
     def _str_(self):
