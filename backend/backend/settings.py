e16b36f17890ff3080a99b074996f0efd70323fa
Артём Берников <bernikovartemal@gmail.com>
2025-10-02 21:53:23 +0200

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,8 @@ ALLOWED_HOSTS = os.getenv(
     'localhost,127.0.0.1,backend'
 ).split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
 
 # Application definition
 
