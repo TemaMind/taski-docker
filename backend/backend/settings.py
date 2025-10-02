0f864e62c8d403598d0eac2c64e5bc116bd315d5
Артём Берников <bernikovartemal@gmail.com>
2025-10-02 20:57:19 +0200

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,9 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', '0') == '1'
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = os.getenv(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1,backend'
+).split(',')
 
 
 # Application definition
